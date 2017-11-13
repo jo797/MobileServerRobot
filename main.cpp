99ad4543b508c642f238cb756a67c2a56ee90337
Joletta Cheung <j.cheung.797@hotmail.com>
2017-11-13 16:33:42 -0500

    created project for robot code

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by joletta on 13/11/17.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by joletta on 13/11/17.
+//
+
+#ifndef MOBILESERVERROBOT_MAIN_H
+#define MOBILESERVERROBOT_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //MOBILESERVERROBOT_MAIN_H
