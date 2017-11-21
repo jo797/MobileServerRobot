7bd2c2f980578f22437ed3a34c72e5013e3e6d6e
bcorbold <bradencorbold@gmail.com>
2017-11-21 14:57:57 -0500

    [chore] Added README and gitignore files [chore] Cleaned up imports

---
+++ README.md
@@ -0,0 +1,2 @@
+# MobileServerRobot
+Welcome to the repo for the **MobileServer** McMaster University Capstone project! This repo contains the code run on the **MobileServer** robot to deliver drinks!
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,18 @@ package com.company;
 
 import java.io.IOException;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+
 import org.json.simple.JSONObject;
 
 
